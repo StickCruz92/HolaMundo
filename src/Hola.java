65ba22db0b59760c4684f3194eb43a3a9d634a84
Stick cruz <stick.cruz1992@gmail.com>
2021-05-13 21:04:20 -0500

    primer commit rama nuevafuncionalidad 3

--- src/Hola.java
+++ src/Hola.java
@@ -4,11 +4,16 @@ public class Hola {
 
 	public static void main(String[] args) {
 	 
+		int cantidad = 10;
+		
 		Scanner in = new Scanner(System.in);  
 		System.out.println("Por favor ingresa un nombre: ");
 		String name = in.nextLine();  
 		
 		System.out.println("Hola Mundo " + name);
  
+		for (int i = 0; i < cantidad; i++) {
+			System.out.println("Hola Mundo " + name + " cantidad " + i);
+		}
 	}
 }
