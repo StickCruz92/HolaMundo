bd77c0fa1b50c830293a1f179756005a90eca141
Stick cruz <stick.cruz1992@gmail.com>
2021-05-13 20:37:28 -0500

    segundo comando

--- src/Hola.java
+++ src/Hola.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Hola {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Munbdo");
+	 
+		Scanner in = new Scanner(System.in);  
+		System.out.println("Por favor ingresa un nombre: ");
+		String name = in.nextLine();  
+		
+		System.out.println("Hola Mundo " + name);
  
 	}
-
 }
